581dc8ca499886c51e00c0ac726ab98a9048a64f
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2021-06-27 11:25:15 +0200

    Install scripts in qt5-base

--- ports/qt5-base/portfile.cmake
+++ ports/qt5-base/portfile.cmake
@@ -1,1 +1,16 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+#install scripts for other qt ports
+file(COPY
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_port_hashes.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_port_functions.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_fix_makefile_install.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_fix_cmake.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_fix_prl.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_download_submodule.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_build_submodule.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_install_copyright.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/qt_submodule_installation.cmake
+    DESTINATION
+        ${CURRENT_PACKAGES_DIR}/share/qt5
+)
